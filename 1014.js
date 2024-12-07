8d1855088a6f31d26949f0fc389301c8689d2dd8
Gabrieli Valentim <gabisvalentim2@gmail.com>
2024-12-06 21:20:21 -0300

    deleted due to wrong path

--- 1014.js
+++
@@ -1,9 +0,0 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n');
-
-let X = parseInt(lines.shift());
-let Y = parseFloat(lines.shift());
-
-let consumo = X / Y;
-
-console.log(`${consumo.toFixed(3)} km/l`)
